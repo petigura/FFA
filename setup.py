ce26a9a59247b3665ab2c11b87c30493a6404725
Erik Petigura <epetigura@berkeley.edu>
2013-02-04 11:07:58 -0800

    changed setup.py to incorporate FBLS algorithms

--- setup.py
+++ setup.py
@@ -6,9 +6,15 @@ import numpy
 ext_modules = [Extension(name="FFA_cy",
                          sources=["FFA_cy.pyx"],
                          include_dirs=[numpy.get_include()]),
+               Extension(name="FBLS_cy",
+                         sources=["FBLS_cy.pyx"],
+                         include_dirs=[numpy.get_include()]),
                Extension(name="FFA_cext",
                          sources=["FFA_cext.pyx","FFA.c"],
+                         include_dirs=[numpy.get_include()]),
+               Extension(name="FBLS_cext",
+                         sources=["FBLS_cext.pyx","FBLS.c"],
                          include_dirs=[numpy.get_include()])
-                         ]
+               ]
 
 setup(cmdclass={'build_ext': build_ext}, ext_modules=ext_modules)
