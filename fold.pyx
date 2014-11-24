0c04c77b558a4196dce906446f4d8aab7280ce1f
Erik Petigura <epetigura@berkeley.edu>
2014-11-24 10:17:45 -0800

    fold_ma2 -> fold_col fold_col ~10x faster

--- fold.pyx
+++ fold.pyx
@@ -10,8 +10,6 @@ from libcpp cimport bool
 import numpy as np
 from numpy import ma
 
-
-
 cpdef np.ndarray[double] fold_sum(np.ndarray[np.int64_t, ndim=1] col,np.ndarray[np.float64_t, ndim=1] x):
     cdef float total
     cdef int icad, icol
@@ -77,6 +75,49 @@ cpdef fold_ma(np.ndarray[np.float64_t, ndim=1] data,
         sscol[icol] = ss_sum
     return ccol,scol,sscol
 
+cpdef fold_col(np.ndarray[np.float64_t, ndim=1] data,
+              np.ndarray[np.int64_t, ndim=1] mask, 
+              np.ndarray[np.int64_t, ndim=1] col):
+    """
+    Fold Columns
+
+    Assign each point of time series to a column (bin) compute the
+    following aggregating statristics for each column.
+
+    Parameters
+    ----------
+    data : (float) data array
+    mask : (int) mask for data array. 1 = masked out.
+    col : (int) column corresponding to phase bin of measurement.
+
+    Return
+    ------
+    ccol : total number of non-masked elements
+    scol : sum of elements
+    sscol : sum of squares of elements
+    """
+
+    cdef int icad, icol,ncad, ncol
+    ncad = data.shape[0]
+    ncol = np.max(col)+1
+    
+    # Define column arrays
+    cdef np.ndarray[np.int64_t] ccol = np.zeros(ncol,dtype=int)
+    cdef np.ndarray[np.float64_t] scol = np.zeros(ncol)
+    cdef np.ndarray[np.float64_t] sscol = np.zeros(ncol)
+
+    # Loop over cadences
+    for icad in range(ncad):
+        if (mask[icad]==0):
+            icol = col[icad]
+
+            # Increment counters
+            ccol[icol]+=1 
+            scol[icol]+=data[icad] 
+            sscol[icol]+=data[icad]**2
+
+    return ccol,scol,sscol
+
 def wrap_icad(icad,Pcad):
     """
     rows and column identfication to each one of the
@@ -92,16 +133,6 @@ def wrap_icad(icad,Pcad):
     col = np.floor(np.mod(icad,Pcad)).astype(int)
     return row,col
 
-cdef int wrap_col(int icad, double Pcad):
-    """
-    Test equivalence between wrap_icad and wrap_col
-
-    col1 = array(map(lambda x : fold.wrap_col(x,3.3),arange(10)))
-    col2 = fold.wrap_icad(arange(10),3.3)
-    """
-    cdef int col
-    col = int ( cython.cmod(icad , Pcad) )
-    return col
     
 
 
