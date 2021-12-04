42a7b6d900250111e1da739007d677a499cbefbf
David Palmer <palmer@lanl.gov>
2021-12-03 18:13:02 -0700

    Automatic cython run; removed cython output from git; test all three modules in unit tests.

--- FFA/FFA_tests.py
+++ FFA/FFA_tests.py
@@ -1,34 +1,41 @@
 from __future__ import print_function
 import unittest
 import numpy as np
-# import FFA_cy as FFA
-import FFA_cext as FFA
+import FFA
+import FFA_cext
+import FFA_cy
 import os
 import datetime
 
 datadir = os.path.dirname(__file__) + '/sample_data/'
 
+FFAlibs = [FFA, FFA_cext, FFA_cy]
 
 class TestClass(unittest.TestCase):
     def test_FFA(self):
-        for input in ['eye-4x6.npy', 'eye-32x2500.npy', 'P_2500+1.npy']:
-            output = 'res_' + input
-            inputArr = np.load(datadir + input)
-            outputArr = np.load(datadir + output)
+        for FFAlib in FFAlibs:
+            print("Testing", FFAlib.__name__)
+            for input in ['eye-4x6.npy', 'eye-32x2500.npy', 'P_2500+1.npy']:
+                output = 'res_' + input
+                inputArr = np.load(datadir + input)
+                outputArr = np.load(datadir + output)
 
-            print(inputArr)
-            print(outputArr)
-            self.assertTrue(np.allclose(FFA.FFA(inputArr), outputArr))
+                print(inputArr)
+                print(outputArr)
+                self.assertTrue(np.allclose(FFAlib.FFA(inputArr), outputArr))
 
     def test_time(self):
-        ntries = 100
-        t0 = datetime.datetime.now()
-        input = datadir + 'eye-32x2500.npy'
-        inputArr = np.load(input)
-        for i in range(ntries):
-            FFA.FFA(inputArr)
-        dt = (datetime.datetime.now() - t0).total_seconds()
-        print("Folding {}x{} array requires {} s (average of {} runs)".format(*inputArr.shape, dt/ntries, ntries))
+        for FFAlib in FFAlibs:
+            print("Testing", FFAlib.__name__)
+            ntries = 100
+            t0 = datetime.datetime.now()
+            input = datadir + 'eye-32x2500.npy'
+            inputArr = np.load(input)
+            for i in range(ntries):
+                FFAlib.FFA(inputArr)
+            dt = (datetime.datetime.now() - t0).total_seconds()
+            print("Folding {}x{} array with {} requires {} s (average of {} runs)".format(
+                *inputArr.shape, FFAlib.__name__, dt/ntries, ntries))
 
 if __name__ == '__main__':
     unittest.main()
--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Distutils import build_ext
+# from Cython.Build import cythonize
 import numpy
 
-ext_modules = [Extension(name="FFA_cy",
+ext_modules = [
+                Extension(name="FFA_cy",
                          sources=["FFA/FFA_cy.pyx"],
-                         include_dirs=[numpy.get_include()]), 
+                         include_dirs=[numpy.get_include()]),
                Extension(name="FBLS_cy",
                          sources=["FFA/FBLS_cy.pyx"],
                          include_dirs=[numpy.get_include()]),
                Extension(name="BLS_cy",
                          sources=["FFA/BLS_cy.pyx"],
                          include_dirs=[numpy.get_include()]),
-              Extension(name="FFA_cext",
-                         sources=["FFA/FFA_cext.pyx","FFA/FFA.c"],
+               Extension(name="FFA_cext",
+                         sources=["FFA/FFA_cext.pyx", "FFA/FFA.c"],
                          include_dirs=[numpy.get_include()]),
                Extension(name="FBLS_cext",
-                         sources=["FFA/FBLS_cext.pyx","FFA/FBLS.c"],
+                         sources=["FFA/FBLS_cext.pyx", "FFA/FBLS.c"],
                          include_dirs=[numpy.get_include()]),
                Extension(name="BLS_cext",
-                         sources=["FFA/BLS_cext.pyx","FFA/BLS.c"],
+                         sources=["FFA/BLS_cext.pyx", "FFA/BLS.c"],
                          include_dirs=[numpy.get_include()]),
                Extension(name="fold",
                          sources=["FFA/fold.pyx"],
                          include_dirs=[numpy.get_include()])
-           ]
+               ]
 
 setup(
     cmdclass={'build_ext': build_ext},
